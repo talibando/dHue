e41fa6ebed93525386bbda666cc67879d8c581d0
flynn <46849064+flyxn@users.noreply.github.com>
2020-08-15 22:20:26 -0700

    Changed name of ownerID to whitelistID ✏️

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,6 @@ module.exports = {
     bridgeUsername: 'bridge_username',  //The username you generated on your Hue Bridge
     apiTimeout: 15000,  //How long the API will wait before it times out, the default is 15 seconds
     whitelistOnly: false,   //If this is toggled only the person with the specified Owner ID can use commands, with the exception of help and ping
-    whitelistID: [], //If ownerOnly is toggled, this is the Discord user ID of the user you want to be able to do all of the commands. This supports multiple ownerIDs
+    whitelistID: [], //If ownerOnly is toggled, this is the Discord user ID of the user you want to be able to do all of the commands. This supports multiple whitelistIDs
     botPrefix: '>'  //The prefix the bot will respond to
 }
